73c6ceeb013fca4dce0ece1fdb6e22cf4542a4e8
slackermorris <morris.w.jack@gmail.com>
2022-05-30 14:47:09 +1200

    feature: extend base tailwind theme, no need for CSS abstractions opted for consistent component and template partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        picnic: ["PicNic", "sans-serif"],
+        helvetica: ["Helvetica"],
+        nhmb: ["NaN Holo Mono Blonde"],
+        sim: ["Suisse Intl Mono"],
+      },
+    },
+    colors: {
+      graphite: {
+        lightest: "#E1DFE1",
+        light: "#C7C7C7",
+        dark: "#54545C",
+        "light-green": "#8C9490",
+        "dark-green": "#545c54",
+      },
+      midnight: {
+        light: "#191919",
+        dark: "#202226",
+      },
+      pink: "#FADAD8",
+      white: "#FFF",
+    },
   },
   plugins: [
     ({ addVariant }) => {
