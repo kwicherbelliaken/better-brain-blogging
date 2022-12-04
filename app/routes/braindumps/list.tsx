aade1d18926cd692edf41475f78add55b74d36e5
slackermorris <morris.w.jack@gmail.com>
2022-12-05 12:15:39 +1300

    feature: CatchBoundary in braindumps list nested route handles for bad requests, etc; ErrorBoundary is a catch all :

--- app/routes/braindumps/list.tsx
+++ app/routes/braindumps/list.tsx
@@ -1,52 +1,153 @@
-import P from "~/components/P";
 import { useEffect, useState } from "react";
-import { Link } from "@remix-run/react";
+import { useCatch } from "@remix-run/react";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/server-runtime";
 import notionClient from "~/integrations/notion";
-import { retrieveBraindumpsFromNotionDatabase } from "./braindumps-helpers";
+import { retrieveBraindumpsFromNotionDatabase } from "./notion-crm";
+
+//? COMPONENTS
+import { Link } from "@remix-run/react";
+import P from "~/components/P";
 import Title from "~/components/Title";
 import FuzzyScrawl from "fuzzy-scrawl";
-import fuzzyScrawlStyles from "fuzzy-scrawl-styles";
 
-// TODO: load the typings for fuzzy-scrawl
-
+//? TYPES
 import type { ThrownResponse } from "@remix-run/react";
 import type {
   HeadersFunction,
   LoaderFunction,
 } from "@remix-run/server-runtime";
 import type { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
-import type { NotionDatabaseAPIMapperResponse } from "./braindumps-helpers";
+import type { NotionDatabaseAPIMapperResponse } from "./notion-crm";
 
-const getRandomIndex = (maxIndex: number) => (index: number) =>
-  index === Math.floor(Math.random() * maxIndex);
+//? STYLES
+import fuzzyScrawlStyles from "fuzzy-scrawl-styles";
 
-const renderBraindumps = (
-  braindump: NotionDatabaseAPIMapperResponse[0],
-  index: number,
-  braindumps: NotionDatabaseAPIMapperResponse
-) => {
-  const braindumpsLength = braindumps.length;
+type ThrownResponses = ThrownResponse<404, string>;
 
-  const showFuzzyScrawl = getRandomIndex(braindumpsLength)(index);
-
-  return (
-    <BraindumpDetails
-      key={index}
-      braindump={braindump}
-      showFuzzyScrawl={showFuzzyScrawl}
-    />
-  );
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  return { "Cache-Control": loaderHeaders.get("Cache-Control")! };
 };
 
-const BraindumpDetails = ({
+export const loader: LoaderFunction = async () => {
+  let httpHeaders = { "Cache-Control": "max-age=3600" };
+
+  const response: QueryDatabaseResponse = await notionClient.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID ?? "",
+  });
+
+  if (!response) {
+    throw json("No Braindumps Found", { status: 404 });
+  }
+
+  return json(retrieveBraindumpsFromNotionDatabase(response), {
+    headers: httpHeaders,
+  });
+};
+
+export function ErrorBoundary({ error }) {
+  return (
+    <div className="flex h-full w-2/3 flex-row items-center justify-center p-24">
+      {error.message}
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch<ThrownResponses>();
+
+  switch (caught.status) {
+    case 404:
+      return (
+        <div className="flex h-full w-2/3 flex-row items-center justify-center p-24">
+          No braindumps were found
+        </div>
+      );
+    default:
+      throw new Error(
+        "Unknown Server error. Likely an unhandled status in 'Catch Boundary'"
+      );
+  }
+}
+
+// TODO: write note about how remix handles styles, particularly for a component library
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: fuzzyScrawlStyles,
+    },
+  ];
+};
+
+export default function BraindumpsList() {
+  const braindumps = useLoaderData() as ReturnType<
+    typeof retrieveBraindumpsFromNotionDatabase
+  >;
+
+  if (!braindumps) return null;
+
+  return (
+    <div className="h-full w-2/3 p-24">
+      <Title.H1 styleProps={["pb-12"]}>braindumps</Title.H1>
+
+      {Object.entries(braindumps).map(
+        (
+          [category, relatedBraindumps]: [
+            category: string,
+            relatedBraindumps: NotionDatabaseAPIMapperResponse
+          ],
+          keyOfCategories: number
+        ) => {
+          return (
+            <div key={keyOfCategories} className="w-full pb-8">
+              <Title.H3
+                styleProps={[
+                  "py-3.5",
+                  "text-4xl",
+                  "font-extrabold",
+                  "uppercase",
+                  "tracking-tight",
+                ]}
+              >
+                {category}
+              </Title.H3>
+
+              <div className="bg-white pl-3.5">
+                {relatedBraindumps.map(
+                  (
+                    braindump: NotionDatabaseAPIMapperResponse[0],
+                    index: number,
+                    braindumps: NotionDatabaseAPIMapperResponse
+                  ) => {
+                    const showFuzzyScrawl =
+                      index === Math.floor(Math.random() * braindumps.length);
+
+                    return (
+                      <BraindumpDetails
+                        key={index}
+                        braindump={braindump}
+                        showFuzzyScrawl={showFuzzyScrawl}
+                      />
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
+}
+
+function BraindumpDetails({
   braindump,
   showFuzzyScrawl,
 }: {
   braindump: any;
   showFuzzyScrawl: boolean;
-}) => {
+}) {
   const [canUseDOM, setCanUseDOM] = useState(false);
 
   useEffect(() => setCanUseDOM(true), []);
@@ -87,99 +188,6 @@ const BraindumpDetails = ({
           <BraindumpName />
         )}
       </div>
-    </div>
-  );
-};
-
-type BraindumpsNotFoundResponse = ThrownResponse<404, string>;
-
-export const headers: HeadersFunction = ({ loaderHeaders }) => {
-  return { "Cache-Control": loaderHeaders.get("Cache-Control")! };
-};
-
-export const loader: LoaderFunction = async () => {
-  try {
-    let httpHeaders = { "Cache-Control": "max-age=3600" };
-
-    const response: QueryDatabaseResponse = await notionClient.databases.query({
-      database_id: process.env.NOTION_DATABASE_ID ?? "",
-    });
-
-    if (!response) {
-      throw json("No Braindumps Found", { status: 404 });
-    }
-
-    return json(retrieveBraindumpsFromNotionDatabase(response), {
-      headers: httpHeaders,
-    });
-  } catch (error) {
-    // [TODO]:
-    // > appropriately handle possible errors here
-  }
-};
-
-export function ErrorBoundary() {
-  // const caught = useCatch<BraindumpsNotFoundResponse>();
-  // console.log("LOGGING INSIDE THE ERROR BOUNDARY: ", { caught });
-  // switch (caught.status) {
-  //   case 404:
-  //     return <div>{caught.statusText}</div>;
-  //   default:
-  //     throw new Error(`Unaccounted for Error: ${caught.statusText}`);
-  // }
-}
-
-// TODO: write note about how remix handles styles, particularly for a component library
-export const links = () => {
-  return [
-    {
-      rel: "stylesheet",
-      href: fuzzyScrawlStyles,
-    },
-  ];
-};
-
-// TODO: relocate all code relevant to this route to this component
-export default function BraindumpsList() {
-  const braindumps = useLoaderData() as ReturnType<
-    typeof retrieveBraindumpsFromNotionDatabase
-  >;
-
-  if (!braindumps) return null;
-
-  return (
-    <div className="h-full w-2/3 p-24">
-      <Title.H1 styleProps={["pb-12"]}>braindumps</Title.H1>
-
-      {Object.entries(braindumps).map(
-        (
-          [category, relatedBraindumps]: [
-            category: string,
-            relatedBraindumps: NotionDatabaseAPIMapperResponse
-          ],
-          keyOfCategories: number
-        ) => {
-          return (
-            <div key={keyOfCategories} className="w-full pb-8">
-              <Title.H3
-                styleProps={[
-                  "py-3.5",
-                  "text-4xl",
-                  "font-extrabold",
-                  "uppercase",
-                  "tracking-tight",
-                ]}
-              >
-                {category}
-              </Title.H3>
-
-              <div className="bg-white pl-3.5">
-                {relatedBraindumps.map(renderBraindumps)}
-              </div>
-            </div>
-          );
-        }
-      )}
     </div>
   );
 }
