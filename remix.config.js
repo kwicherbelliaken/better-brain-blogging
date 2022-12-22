bb6907855bed5097eeecf637ad95dd7fa6583d0d
slackermorris <morris.w.jack@gmail.com>
2022-12-23 10:53:17 +1300

    feature: enabled test routes (the grunge boilerplate was broken)

--- remix.config.js
+++ remix.config.js
@@ -15,20 +15,18 @@ module.exports = {
     return defineRoutes((route) => {
       if (process.env.NODE_ENV === "production") return;
 
-      /* TODO 
-      > restore this test route creation, not sure why it is erroring.. though perhaps path '__tests/create-user' not existing might have something to do
-        with it.
-        
-      > [READ THIS]: https://remix.run/docs/en/v1/guides/routing
-      */
-
       console.log("⚠️  Test routes enabled.");
-      // route(
-      //   "__tests/create-user",
-      //   path.join(__dirname, "cypress/support/test-routes/create-user.ts")
-      // );
+
+      let appDir = path.join(__dirname, "app");
+
+      route(
+        "__tests/create-user",
+        path.relative(appDir, "cypress/support/test-routes/create-user.ts")
+      );
     });
   },
   sourcemap: true,
   serverDependenciesToBundle: ["fuzzy-scrawl"],
 };
+
+//? what we've got is an inability to discover the file
