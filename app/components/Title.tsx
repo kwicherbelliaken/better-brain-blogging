c3d0387816f6ec0f20f319c16155afa8814209bf
slackermorris <morris.w.jack@gmail.com>
2023-02-04 12:50:38 +1300

    feature: point to entry point .css file to apply custom styles--currently being applied to <h> tags

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -5,46 +5,23 @@ const H1 = ({
   children,
   styleProps,
 }: PropsWithChildren<{ styleProps?: Array<string> }>) => (
-  <h1
-    className={classNames(
-      "font-picnic text-9xl text-midnight-light",
-      styleProps?.join(" ")
-    )}
-  >
-    {children}
-  </h1>
+  <h1 className={classNames(styleProps?.join(" "))}>{children}</h1>
 );
 
-const H2 = ({ children }: PropsWithChildren<{}>) => (
-  <h2 className="pb-8 font-nhmb sm:text-8xl">{children}</h2>
-);
+const H2 = ({ children }: PropsWithChildren<{}>) => <h2>{children}</h2>;
 
 const H3 = ({
   children,
   styleProps,
 }: PropsWithChildren<{ styleProps?: Array<string> }>) => (
-  <h3
-    className={classNames(
-      "font-helvetica text-midnight-light sm:text-6xl",
-      styleProps?.join(" ")
-    )}
-  >
-    {children}
-  </h3>
+  <h3 className={classNames(styleProps?.join(" "))}>{children}</h3>
 );
 
 const H4 = ({
   children,
   styleProps,
 }: PropsWithChildren<{ styleProps?: Array<string> }>) => (
-  <h4
-    className={classNames(
-      "font-helvetica text-midnight-light sm:text-4xl",
-      styleProps?.join(" ")
-    )}
-  >
-    {children}
-  </h4>
+  <h4 className={classNames(styleProps?.join(" "))}>{children}</h4>
 );
 
 export default { H1, H2, H3, H4 };
