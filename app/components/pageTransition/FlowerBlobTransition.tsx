931344247e6699b296335befea5e63c3264dccbb
slackermorris <morris.w.jack@gmail.com>
2023-03-24 11:08:05 +1300

    chore: fixing merge conflicts up between this and master

--- app/components/pageTransition/FlowerBlobTransition.tsx
+++ app/components/pageTransition/FlowerBlobTransition.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import classNames from "classnames";
 import anime from "animejs";
 
 import type { PropsWithChildren } from "react";
--- app/components/pageTransition/MorphingTransition.tsx
+++ app/components/pageTransition/MorphingTransition.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import classNames from "classnames";
 import anime from "animejs";
 
 import type { PropsWithChildren } from "react";
